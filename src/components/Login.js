18d0312f0219f12c45ee874ec3568d6bd8230a08
MakenaG <makenaglory44@gmail.com>
2023-03-24 05:30:42 +0300

    updated css

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 
+
 function Login({ handleLogin }) {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
@@ -10,21 +11,25 @@ function Login({ handleLogin }) {
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <input
-        type="text"
-        placeholder="Username"
-        value={username}
-        onChange={(event) => setUsername(event.target.value)}
-      />
-      <input
-        type="password"
-        placeholder="Password"
-        value={password}
-        onChange={(event) => setPassword(event.target.value)}
-      />
-      <button type="submit">Log In</button>
-    </form>
+    <>
+      <h2 className="title">To-Do List App</h2>
+      {/* <p className="subtitle">Please log in to view your todos</p> */}
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Username"
+          value={username}
+          onChange={(event) => setUsername(event.target.value)}
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(event) => setPassword(event.target.value)}
+        />
+        <button type="submit" className="login-btn">Log In</button>
+      </form>
+    </>
   );
 }
 
